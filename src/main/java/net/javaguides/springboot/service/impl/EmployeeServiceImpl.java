de2364b5721073cdcb41cd25443f65567456af1d
rahul-v-t <rahulthambi10@gmail.com>
2021-12-29 11:11:41 -0800

    new spring project

---
+++ src/main/java/net/javaguides/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,61 @@
+package net.javaguides.springboot.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import net.javaguides.springboot.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	private EmployeeRepository employeeRepository;
+	
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		super();
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> employee = employeeRepository.findById(id);
+		if(employee.isPresent()) {
+			return employee.get();
+		}else {
+			throw new ResourceNotFoundException("Employee","Id",id);
+		}
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee, long id) {
+		
+		
+		//check whether employee with given id is exist in or not
+		Employee existingEmployee = employeeRepository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Employee", "Id", id));
+		
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setEmail(employee.getEmail());
+		
+		//save existing employee to DB
+		
+		employeeRepository.save(existingEmployee);
+		
+		return existingEmployee;
+	}
+
+}
